27156f9ced0b103b654c69af253de557cc69cd09
Stevek <vegh@spsejecna.cz>
2025-12-16 10:00:46 +0100

    bump plugin foojay-resolver-convention to 1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "jecnaapi"
 include("jecnaapi-java")
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 dependencyResolutionManagement {
