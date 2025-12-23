7ab16e3fba2302c5fac8f8eee2e2435820a5250d
stevek <vegh@spsejecna.cz>
2025-12-23 20:39:22 +0100

    add classroom ref and ser

---
+++ src/main/kotlin/io/github/tomhula/jecnaapi/data/classroom/ClassroomReference.kt
@@ -0,0 +1,36 @@
+﻿package io.github.tomhula.jecnaapi.data.classroom
+
+import io.github.tomhula.jecnaapi.serialization.ClassroomReferenceSerializer
+import kotlinx.serialization.Serializable
+
+@Serializable(with = ClassroomReferenceSerializer::class)
+class ClassroomReference(
+    val title: String,
+    val floor: String,
+)
+{
+    override fun equals(other: Any?): Boolean
+    {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ClassroomReference
+
+        if (title != other.title) return false
+        if (floor != other.floor) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int
+    {
+        var result = title.hashCode()
+        result = 31 * result + floor.hashCode()
+        return result
+    }
+    
+    override fun toString(): String
+    {
+        return "ClassroomReference(title='$title', floor='$floor')"
+    }
+}
---
+++ src/main/kotlin/io/github/tomhula/jecnaapi/serialization/ClassroomReferenceSerializer.kt
@@ -0,0 +1,24 @@
+﻿package io.github.tomhula.jecnaapi.serialization
+
+import io.github.tomhula.jecnaapi.data.classroom.ClassroomReference
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+internal object ClassroomReferenceSerializer: KSerializer<ClassroomReference>
+{
+    override val descriptor = PrimitiveSerialDescriptor("ClassroomReference", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: ClassroomReference)
+    {
+        encoder.encodeString(value.title+ "$" + value.floor)
+    }
+
+    override fun deserialize(decoder: Decoder): ClassroomReference
+    {
+        val split = decoder.decodeString().split("$")
+        return ClassroomReference(split[0], split[1])
+    }
+}
