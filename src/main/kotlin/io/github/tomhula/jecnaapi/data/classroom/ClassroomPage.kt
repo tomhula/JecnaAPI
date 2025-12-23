90913fff2670704f661256a0745f28531e70e8ed
stevek <vegh@spsejecna.cz>
2025-12-23 20:43:25 +0100

    add classroom page

---
+++ src/main/kotlin/io/github/tomhula/jecnaapi/data/classroom/ClassroomPage.kt
@@ -0,0 +1,25 @@
+﻿package io.github.tomhula.jecnaapi.data.classroom
+
+import io.github.tomhula.jecnaapi.data.schoolStaff.TeacherReference
+
+class ClassroomPage(val classroomRefs: Set<TeacherReference>)
+{
+    companion object
+    {
+        @JvmStatic
+        fun builder() = Builder()
+    }
+
+    class Builder
+    {
+        private val classroomRefs = mutableSetOf<TeacherReference>()
+
+        fun addClassroomReference(classroomRef: TeacherReference): Builder
+        {
+            classroomRefs.add(classroomRef)
+            return this
+        }
+
+        fun build() = ClassroomPage(classroomRefs)
+    }
+}
