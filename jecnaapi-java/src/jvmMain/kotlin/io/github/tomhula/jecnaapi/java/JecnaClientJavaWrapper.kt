2ba2eb9257c67347d4f1a836847ee8398827be08
Tomas Hula <tomashula06@gmail.com>
2026-01-20 19:22:17 +0100

    Fix getRoom future wrapping in JecnaClientJavaWrapper

--- jecnaapi-java/src/jvmMain/kotlin/io/github/tomhula/jecnaapi/java/JecnaClientJavaWrapper.kt
+++ jecnaapi-java/src/jvmMain/kotlin/io/github/tomhula/jecnaapi/java/JecnaClientJavaWrapper.kt
@@ -87,9 +87,9 @@ class JecnaClientJavaWrapper(autoLogin: Boolean = false)
     
     fun getRoomsPage() = GlobalScope.future { wrappedClient.getRoomsPage() }
     
-    fun getRoom(roomReference: RoomReference) = GlobalScope.future { GlobalScope.future { wrappedClient.getRoom(roomReference) } }  
+    fun getRoom(roomReference: RoomReference) = GlobalScope.future { wrappedClient.getRoom(roomReference) }
     
-    fun getRoom(roomCode: String) = GlobalScope.future { GlobalScope.future { wrappedClient.getRoom(roomCode) } }  
+    fun getRoom(roomCode: String) = GlobalScope.future { wrappedClient.getRoom(roomCode) } 
 
     /** A query without any authentication (autologin) handling. */
     fun plainQuery(path: String, parameters: Parameters? = null) =
