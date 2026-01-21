14b3fed9f1b7989949fd6111711fe7a32d1d710e
Tomas Hula <tomashula06@gmail.com>
2026-01-21 09:59:14 +0100

    Make Notification serializable

--- src/commonMain/kotlin/io/github/tomhula/jecnaapi/data/notification/Notification.kt
+++ src/commonMain/kotlin/io/github/tomhula/jecnaapi/data/notification/Notification.kt
@@ -2,10 +2,12 @@ package io.github.tomhula.jecnaapi.data.notification
 
 import io.github.tomhula.jecnaapi.data.schoolStaff.TeacherReference
 import kotlinx.datetime.LocalDate
+import kotlinx.serialization.Serializable
 
 /**
  * Represents a single notification about a student's behaviour or general information for parents.
  */
+@Serializable
 data class Notification(
     val notificationType: NotificationReference.NotificationType,
     val exactType: String,
